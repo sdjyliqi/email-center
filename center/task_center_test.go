dc3615e71d3ae1a9985cd42a8c2298015205563e
liqi <sdjyliqi@163.com>
2021-08-03 17:29:08 +0800

    继续优化body

--- center/task_center_test.go
+++ center/task_center_test.go
@@ -1,7 +1,22 @@
 package center
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/DusanKasan/parsemail"
+	"io/ioutil"
+	"testing"
+)
 
 func TestTaskCenter(t *testing.T) {
-	TaskCenter()
+	//TaskCenter()
+	contents, err := ioutil.ReadFile("D:\\gowork\\src\\email-center\\data\\发票类\\异常\\20210713000936__yqsidg@ukkl.com__202107130009361898199814091494693.eml")
+	//reader.Read(contents)
+	emailReader := bytes.NewReader(contents)
+	fmt.Println(emailReader)
+	email, err := parsemail.Parse(emailReader) // returns Email struct and error
+	fmt.Println("====", email.Subject)
+	b, err := json.Marshal(email)
+	t.Log(email.Subject, string(b), err)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module email-center
 go 1.14
 
 require (
+	github.com/DusanKasan/parsemail v1.2.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/go-xorm/xorm v0.7.9
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
