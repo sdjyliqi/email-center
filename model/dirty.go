4dd6bd80c6ca52ee15c75418f6793c39c076239b
liqi <sdjyliqi@163.com>
2021-10-29 21:09:14 +0800

    bug 修改数据早点return

--- model/dirty.go
+++ model/dirty.go
@@ -80,6 +80,7 @@ func (t Dirty) UpdateItemByID(item *Dirty) error {
 			glog.Errorf("Update item %+v from %s failed,err:%+v", *item, t.TableName(), err)
 			return err
 		}
+		return nil
 	}
 	item.Id = 0
 	item.Category = "色情"
